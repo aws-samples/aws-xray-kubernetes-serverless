dc4e7fa27ade71eca3bccbcc748d33f83a5f3ce3
Kumar <bharathkv@users.noreply.github.com>
2018-08-07 14:09:22 -0700

    Serverless should be working now with DDB

--- lambda/index.js
+++ lambda/index.js
@@ -4,7 +4,7 @@ var logger = require('winston');
 AWSXRay.setLogger(logger);
 var AWS = AWSXRay.captureAWS(require('aws-sdk'));
 
-var ddb = new AWS.DynamoDB();
+var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 
 exports.handler = (event, context, callback) => {
     var table = "xrayk8s";
@@ -22,9 +22,9 @@ exports.handler = (event, context, callback) => {
     var params = {
     TableName:table,
         Item:{
-            "id":{N:""+id+""},
-            "message": {S:message},
-            "username": {S:username}
+            "id":id,
+            "message": message,
+            "username": username
         }
     };
 
@@ -32,7 +32,7 @@ console.log("Adding a new message to DynamoDB: "+message);
 response.message = "Adding a new message to DynamoDB: "+message;
 
 AWSXRay.captureAsyncFunc("## dynamodb", function(subsegment) {
-    ddb.putItem(params, function(err, data) {
+    docClient.put(params, function(err, data) {
         if (err) {
             console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
             response.message = "Unable to add item. Error JSON:", JSON.stringify(err, null, 2);
@@ -40,11 +40,11 @@ AWSXRay.captureAsyncFunc("## dynamodb", function(subsegment) {
             console.log("Added item:", JSON.stringify(data, null, 2));
             response.message = "Added item:", JSON.stringify(data, null, 2);
         }
+
+        subsegment.close();
     });
     subsegment.addAnnotation("username",username);
     subsegment.addAnnotation("message",message);
-
-    subsegment.close();
 });
 
     callback(null, response);
